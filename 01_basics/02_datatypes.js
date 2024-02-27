c42c89435a5a3cb72a36063ae36c408493355d7e
Kanisha Jain <122852747+kanishajain@users.noreply.github.com>
2024-02-27 10:11:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";   //treat all JS code as newer version
+
+// alert(3+3)   //it will throw error because we are using node js, not browser
+
+let name="kanisha"
+let age=20;
+let isLoggedIn=true;
+let state;
+
+
+//PRIMITIVE DATATYPES
+
+// number => range from 2 to 2^53
+// BigInt => for large numbers
+// string => " "
+// boolean =>true or false
+// null =>standalone value
+// undefined =>
+// symbol =>unique
+
+// NON-PRIMITIVE DATATYPES
+
+// object
+
+
+// typeof operator
+console.log(typeof "kanisha")   //string
+console.log(typeof undefined)   //undefined
+console.log(typeof null)      //object
